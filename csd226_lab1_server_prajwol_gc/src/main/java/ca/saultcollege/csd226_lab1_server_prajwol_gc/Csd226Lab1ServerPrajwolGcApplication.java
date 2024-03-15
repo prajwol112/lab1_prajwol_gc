fbb46ea7a1ba2e0c04a75db2cccd49dd3610e092
Prajwol G C <gcprajwol25@gmail.com>
2024-03-15 10:14:30 -0400

    Initial commit

---
+++ csd226_lab1_server_prajwol_gc/src/main/java/ca/saultcollege/csd226_lab1_server_prajwol_gc/Csd226Lab1ServerPrajwolGcApplication.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.csd226_lab1_server_prajwol_gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csd226Lab1ServerPrajwolGcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csd226Lab1ServerPrajwolGcApplication.class, args);
+	}
+
+}
---
+++ csd226_lab1_server_prajwol_gc/src/test/java/ca/saultcollege/csd226_lab1_server_prajwol_gc/Csd226Lab1ServerPrajwolGcApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.csd226_lab1_server_prajwol_gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csd226Lab1ServerPrajwolGcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
